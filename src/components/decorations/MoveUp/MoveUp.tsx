b1658cb006208ac6aa0830ef1b3bbb42ad28a52b
overlife3 <vip.kostya.semenov.2004@mail.ru>
2023-08-16 22:12:15 +0300

    move_up

--- src/components/decorations/MoveUp/MoveUp.tsx
+++ src/components/decorations/MoveUp/MoveUp.tsx
@@ -2,8 +2,12 @@ import React from "react";
 import SvgSelector from "../../SvgSelector/SvgSelector";
 import style from "./MoveUp.module.scss";
 function MoveUp() {
+  const handleClick = () => {
+    document.querySelector("body")?.scrollIntoView({ behavior: "smooth" });
+  };
+
   return (
-    <div className={style.move_up}>
+    <div className={style.move_up} onClick={handleClick}>
       <SvgSelector name="down" />
     </div>
   );
